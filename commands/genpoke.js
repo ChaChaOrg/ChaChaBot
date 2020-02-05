c9b56fed606e46ac248f01045350b52839cfa29c
Talmanaze <j.zyborg@gmail.com>
2020-02-04 21:52:58 -0500

    added a .toLowercase on poke species name lmao

--- commands/genpoke.js
+++ commands/genpoke.js
@@ -8,7 +8,7 @@ module.exports.run = (client, connection, P, message, args) => {
 		message.reply('New Pokemon Generator. Variables in order:\n [Pokemon Species] [Level] [Pokemon Name] [Hidden Ability % (optional - CURRENTLY BROKEN)]').catch(console.error);
 	}
 	try{
-		let genPokemon = new Pokemon.Pokemon(args[0], args[1], args[2]);
+		let genPokemon = new Pokemon.Pokemon(args[0].toLowerCase(), args[1], args[2]);
 		let genPromise = genPokemon.init(P, message);
 		genPromise
 			.then( function(response){
