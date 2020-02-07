19583666587da07cd631f4067b04e203433c56a9
Talmanze <j.zyborg@gmail.com>
2020-02-07 16:51:29 -0500

    sql Prep

--- pokemon.js
+++ pokemon.js
@@ -487,15 +487,15 @@ Pokemon.prototype.uploadPokemon = function(connection, message) {
         `hpEV, atkEV, defEV, spaEV, spdEV, speEV, ` +
         `move1, move2, move3, move4, move5, moveProgress, ` +
         `originalTrainer, userID, dateCreated) ` +
-        `VALUES (${this.name}, ${this.species}, ${this.level}, ${this.natureFinal}, ${this.gender}, ${this.ability.name},` +
+        `VALUES ("${this.name}", "${this.species}", ${this.level}, "${this.natureFinal}", "${this.gender}", "${this.ability.name}",` +
         `${this.finalStats[HP_ARRAY_INDEX]}, ${this.finalStats[ATK_ARRAY_INDEX]}, ${this.finalStats[DEF_ARRAY_INDEX]}, ` +
         `${this.finalStats[SPA_ARRAY_INDEX]}, ${this.finalStats[SPD_ARRAY_INDEX]}, ${this.finalStats[SPE_ARRAY_INDEX]}, ` +
         `${this.ivStats[HP_ARRAY_INDEX]}, ${this.ivStats[ATK_ARRAY_INDEX]}, ${this.ivStats[DEF_ARRAY_INDEX]}, ` +
         `${this.ivStats[SPA_ARRAY_INDEX]}, ${this.ivStats[SPD_ARRAY_INDEX]}, ${this.ivStats[SPE_ARRAY_INDEX]}, ` +
         `${this.evStats[HP_ARRAY_INDEX]}, ${this.evStats[ATK_ARRAY_INDEX]}, ${this.evStats[DEF_ARRAY_INDEX]}, ` +
         `${this.evStats[SPA_ARRAY_INDEX]}, ${this.evStats[SPD_ARRAY_INDEX]}, ${this.evStats[SPE_ARRAY_INDEX]}, ` +
-        `${this.move1}, ${this.move2}, ${this.move3}, ${this.move4}, ${this.move5}, ${this.moveProgress}, ` +
-        `${this.originalTrainer}, ${message.author.id}, ${this.dateCreated})`;
+        `"${this.move1}", "${this.move2}", "${this.move3}", "${this.move4}", "${this.move5}", ${this.moveProgress}, ` +
+        `"${this.originalTrainer}", ${message.author.id}, "${this.dateCreated})"`;
 
     console.log(sql);
     connection.query(sql, function (err, result) {
