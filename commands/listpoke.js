a7322a47c7717bd2bd5d14da2bd74f6554c8a8be
Talmanze <j.zyborg@gmail.com>
2020-02-08 00:54:53 -0500

    sql Prep

--- commands/listpoke.js
+++ commands/listpoke.js
@@ -1,12 +1,14 @@
 module.exports.run = (client, connection, P, message, args) => {
 
+    message.author.dmChannel.send("test");
     connection.query("SELECT * FROM pokemon", function (err, result) {
         let printString = "";
         result.forEach(element => {
             printString += `${element.name} (Level ${element.level} ${element.species})\n`;
         });
         message.channel.send(printString);
-        message.author.dmChannel.send("test\n" + printString);
-    }.bind(this, message));
+
+    });
+
 
 };
