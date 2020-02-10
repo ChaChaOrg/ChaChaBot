0bf38ea571af4f085829b9a5fc42b25eafa6be67
Talmanze <j.zyborg@gmail.com>
2020-02-09 19:47:20 -0500

    Started Import and damage.js

--- commands/importpoke.js
+++ commands/importpoke.js
@@ -6,5 +6,5 @@ module.exports.run = (client, connection, P, message, args) => {
 
     if(args[0] === "help") message.reply('Pokemon importer. Paste a pokemon showdown export string. DO NOT FORGET ANY FIELDS. DOES NOT HANDLE ALL VARIATIONS').catch(console.error);
     else console.log(message.content.substr("+pokeimport ".length, message.content.length - "+pokeimport ".length));
-    //importPoke.importPokemon(message.content.substr("+pokeimport ".length, message.content.length - "+pokeimport ".length));
+    importPoke.importPokemon(message.content.substr("+pokeimport ".length, message.content.length - "+pokeimport ".length));
 };
