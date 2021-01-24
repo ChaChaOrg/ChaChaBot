2b657d4ae561a54fc6227716cd65b4b9e6e16472
cdevo <cdevo@DESKTOP-219J7PT>
2021-01-24 15:32:25 -0500

    NeoMoveTutor Update
    Jsdom dependency added
    Logger functionality added, uncertain if it works

--- commands/neomovetutor.js
+++ commands/neomovetutor.js
@@ -3,6 +3,7 @@
 
 const databaseURL = "https://bulbapedia.bulbagarden.net/wiki/List_of_moves";
 const PPINDEX = 11;
+const logs = require('../logs/logger.js');
 
 exports.run = (client, connection, P, message, args) => {
 
@@ -10,14 +11,18 @@ exports.run = (client, connection, P, message, args) => {
 	let jsdom = require('jsdom');
 	
 	if(args[0].includes('help')){
+		logs.info("[neomovetutor] Sending help message");
 		message.channel.send("The command to check the Train Pokemon DC to learn a new move.\n\n+neomovetutor [MoveName (Use _'s for spaces plz)]\n\nTo learn a skill: +neomovetutor Skillpoint [PokeName] [IntMod] [SkillName]\n")
 		return;
 	}if ( args[0].toLowerCase().includes('skillpoint')) { //check if asking for a skill; if so, return skill dc
+		logs.info("[neomovetutor] Skill tutor calculations");
 		var skillDC = 20 - args[2];
 		message.channel.send(`The DC for ${args[1]} to learn the ${args[3]} skill is ${skillDC}.`).catch(console.error);
 		return;
 	} else {
+		logs.info("[neomovetutor] Move tutor calculations");
 		//var request = new XMLHttpRequest();
+		logs.info("[neomovetutor] Sending https request");
 		https.get(databaseURL, (response) =>{
 			if(response.statusCode == 200){
 				
@@ -27,6 +32,7 @@ exports.run = (client, connection, P, message, args) => {
 				});
 				
 				response.on('end', () => {					
+					logs.info("[neomovetutor] Response recieved");
 					let moveName = args[0];
 					moveName = moveName.replace("_", " ");
 					//var selectorString = ":contains('" + moveName + "')";
@@ -46,17 +52,20 @@ exports.run = (client, connection, P, message, args) => {
 						let ppCell = row.childNodes[PPINDEX];
 						let pp = ppCell.innerHTML;//read pp from
 						if(pp && !Number.isNaN(pp)){
-							let DCs = [20,17,15,13,10,8];
+							logs.info("[neomovetutor] Calculating DC");
+							let DCs = [20, 17, 15, 13, 10, 8];
 							pp = parseInt(pp);
 							let dcAdjust = 8-Math.round(pp/5);
 							let output = "";
 							if(args.length >= 2 && args[1].includes("origin")){
-								DCs = [20 + dcAdjust,17 + dcAdjust,15 + dcAdjust,15,13,10]; 
+								logs.info("[neomovetutor] Adjusting to original formula");
+								DCs = [20 + dcAdjust, 17 + dcAdjust, 15 + dcAdjust, 15, 13, 10]; 
 							}
 							
 							for(let i = 0; i < DCs.length; i++){
 								DCs[i] += dcAdjust;
 							}
+							logs.info("[neomovetutro] Displaying results");
 							output += "**Out of Combat Checks** (Checks 1-3)\n";
 							output += "Use your trainer's cha modifier for these checks.\n";
 							output += "```First DC: " + DCs[0] + ", " + "Second DC: " + DCs[1] + ", " + "Third DC: " + DCs[2] + "```\n\n";
@@ -67,9 +76,11 @@ exports.run = (client, connection, P, message, args) => {
 							output += "You get a +2 to the check if it's the second evolution stage that can learn the move.\n";
 							message.channel.send(output);
 						}else{
+							logs.error("[neomovetutor] " + moveName + " found, could not locate pp value");
 							message.channel.send("Could not find pp of move: " + moveName);
 						}		
 					}else{
+						logs.error("[neomovetutor] Unable to find the move " + moveName);
 						message.channel.send("Could not find move: " + moveName);
 					}
 				});
@@ -77,8 +88,9 @@ exports.run = (client, connection, P, message, args) => {
 							
 			}else{
 				//message.channel.send("Couldn't connect to move list(" + databaseURL + ").");
+				logs.error("[neomovetutor] Couldn't connect to move database on bulbapedia, response code: " + response);
 				message.channel.send("Couldn't connect to move list.");
-				console.log("response code: " + response);
+				//console.log("response code: " + response);
 			}
 		});
 		//request.open('GET', "https://bulbapedia.bulbagarden.net/wiki/List_of_moves", false);
