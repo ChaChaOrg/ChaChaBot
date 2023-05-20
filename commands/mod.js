877d77ac7aa9438946c1fdaba517c8f9faed370d
Talmanaze <j.zyborg@gmail.com>
2023-05-20 16:57:02 -0400

    initial mod slash command

--- commands/mod.js
+++ commands/mod.js
@@ -2,17 +2,19 @@ const logger = require('../logs/logger.js');
 
 // JavaScript Document
 // takes a number, gives the mod
+module.exports.data = new SlashCommandBuilder()
+		.setName('mod')
+		.setDescription('Quick Mod Calculator')
+		.addIntegerOption(option =>
+			option.setName('stat')
+				.setDescription('stat you wanted to calculate')
+				.setRequired(true));
 
-exports.run = (interaction) => {
 
-	if (args.length > 1) {
-		logger.info("[mod] Sending too many args interaction.")
-		interaction.channel.send("Woahh, slow down pardner. Just give me one number and I'll give you its modifier.");
-		return;
-	}
+module.exports.run = async (interaction) => 
 
 	//interaction.channel.send('Bang! <:gunspurr:356191158017196032>').catch(console.error);
-	var score = args[0];
+	var score = interaction.options.getInteger('stat');
 	if (score % 2 !== 0) { score = score - 1; } //lower odd numbers by 1
 	var rawMod = ((score - 10) / 2);
 	rawMod = rawMod.toFixed(0);
