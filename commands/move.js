a5d239c9b7e3c2f3d52aa05bd07d5968ec0dcc0e
CheetahX24 <wsauten@hotmail.com>
2023-07-05 18:53:48 -0400

    Added (broken) code for Beat Up and Assist

--- commands/move.js
+++ commands/move.js
@@ -30,45 +30,12 @@ module.exports.data = new SlashCommandBuilder()
 		.addSubcommand(subcommand =>
 			subcommand
 				.setName('beatup')
-				.setDescription('Calculate damage for beat up based on user and applicable Pokemon in party. All must be in bot.')
-				/*
+				.setDescription('Calculate damage for beat up base power for given Pokemon. Must be in bot.')
 				.addStringOption(option =>
-					option.setName('attacker-name')
-					  .setDescription('The name of the attacker, as listed in the database')
-					  .setRequired(true))
-				.addStringOption(option => 
-					option.setName('move-name')
-					  .setDescription('The move used (gen 1-7 only sorry :<) lowercase with dashes instead of spaces. Ie, "rock-smash"')
-					  .setRequired(true))
-				.addStringOption(option => 
-					option.setName('defender-name')
-					  .setDescription('The name of the pokemon being hit by the attack, as listed in the database')
-					  .setRequired(true))
-				.addBooleanOption(option =>
-					option.setName('critical-hit')
-					.setDescription('If the attacker struck a critical hit Defaults to no.'))
-				.addIntegerOption(option =>
-					  option.setName('stages-of-attack')
-					  .setDescription('Stages of attack/special attack the attacker has. Minimum -6, maximum +6')
-					  .setMaxValue(6)
-					  .setMinValue(-6))
-				.addIntegerOption(option =>
-					  option.setName('stages-of-defense')
-					  .setDescription('Stages of defense/special defense the defender has. Minimum -6, maximum +6')
-					  .setMaxValue(6)
-					  .setMinValue(-6))
-				.addIntegerOption(option =>
-					  option.setName('additive-bonus')
-					  .setDescription('Extra damage *added* to the base power. Usually done through ChaCha feats. Defaults to 0'))
-				.addIntegerOption(option =>
-					  option.setName('multiplicitive-bonus')
-					  .setDescription('Extra damage *multiplying* the base power.')
-					  .setMinValue(0))
-				.addStringOption(option =>
-					option.setName('party-members')
-					.setDescription('Party members aiding in Beat Up, seperated by a space')
-					.setRequired(true))*/)
-
+					option.setName('beatuppokemon')
+						.setDescription('Names of Pokemon to calculate base power, seperated by spaces - must be in bot.')
+						.setRequired(true))
+				)
 		.addSubcommand(subcommand =>
 			subcommand
 				.setName('assist')
@@ -115,7 +82,23 @@ module.exports.run = async (interaction) => {
 		["Plasma Fists", "Secret Sword", "Shell Trap", "Sketch"],
 		["Snap Trap", "Snatch", "Spectral Thief", "Spotlight"],
 		["Strange Steam", "Sunsteel Strike", "Techno Blast", "Thousand Arrows"],
-		["Thousand Waves", "V-create"]
+		["Thousand Waves", "V-create", "Metronome"]
+	];
+
+	var assistunselectable = [
+		["Baneful Bunker", "Beak Blast", "Belch", "Bestow"],
+		["Bounce", "Celebrate", "Chatter", "Circle Throw"],
+		["Copycat", "Counter", "Covet", "Destiny Bond"],
+		["Detect", "Dig", "Dive", "Dragon Tail"],
+		["Endure", "Feint", "Fly", "Focus Punch"],
+		["Follow Me", "Helping Hand", "Hold Hands", "King's Shield"],
+		["Mat Block", "Me First", "Metronome", "Mimic"],
+		["Mirror Coat", "Mirror Move", "Nature Power", "Phantom Force"],
+		["Protect", "Rage Powder", "Roar", "Shadow Force"],
+		["Shell Trap", "Sketch", "Sky Drop", "Sleep Talk"],
+		["Snatch", "Spiky Shield", "Spotlight", "Struggle"],
+		["Switcheroo", "Thief", "Transform", "Trick"],
+		["Whirlwind", "Assist"]
 	];
 
 	if (interaction.options.getSubcommand() === 'metronome') {
@@ -166,11 +149,109 @@ module.exports.run = async (interaction) => {
 				}
 			});
 	} else if (interaction.options.getSubcommand() === 'beatup') {
+		let Pokemon = require(`../models/pokemon`);
+        let tempPoke = new Pokemon;
 
-	} else if (interaction.options.getSubcommand() === 'assist') {
+		let followup = "";
+		let inputstring = interaction.options.getString('beatuppokemon');
+		let names = inputstring.split(" ");
 
+		names.forEach((element) => {
+			let notFoundMessage = element + " not found. Please check that you entered the name properly (case-sensitive) and try again.\n\n(Hint: use `+listpoke` to view the Pokemon you can edit.)";
+			let sql = `SELECT * FROM pokemon WHERE name = '${element}';`;
+			logger.info('[move-beatup] SQL query: ${sql}');
+			interaction.client.mysqlConnection.query(sql, function (err, response) {
+				if (err) throw err;
+	
+				if (response.length == 0) {
+					logger.info("[move-beatup] Pokemon not found in database. Please check your spelling, or the Pokemon may not be there.")
+					followup += notFoundMessage + "\n";
+				}
+				else {
+
+					tempPoke.loadFromSQL(interaction.client.mysqlConnection, interaction.client.pokedex, response[0])
+						.then(response => {
+	
+							logger.info("[move-beatup] Got Pokemon info.");
+							followup += "Beat Up base power for " + element + " is " + tempPoke.statBlock.finalStats[1]/10 + 5 + ".\n";
+	
+						});
+				}
+			})
+	});
+		interaction.followUp(followup);
+	}else if (interaction.options.getSubcommand() === 'assist') {
+		let Pokemon = require(`../models/pokemon`);
+        let tempPoke = new Pokemon;
+
+		let followup = "";
+		let partynames = interaction.options.getString('party-members');
+		let names = partynames.split(" ");
+		let movelist = [];
+		names.forEach((element) => {
+			let notFoundMessage = element + " not found. Please check that you entered the name properly (case-sensitive) and try again.\n\n(Hint: use `+listpoke` to view the Pokemon you can edit.)";
+			let sql = `SELECT * FROM pokemon WHERE name = '${element}';`;
+			logger.info('[move-assist] SQL query: ${sql}');
+			interaction.client.mysqlConnection.query(sql, function (err, response) {
+				if (err) throw err;
+	
+				if (response.length == 0) {
+					logger.info("[move-assist] Pokemon not found in database. Please check your spelling, or the Pokemon may not be there.")
+					followup += notFoundMessage + "\n";
+				}
+				else {
+
+					tempPoke.loadFromSQL(interaction.client.mysqlConnection, interaction.client.pokedex, response[0])
+						.then(response => {
+	
+							logger.info("[move-assist] Got Pokemon info.");
+							for (let i = 0; i < 4; i++){
+								//figure out best way to get moves.
+								if (tempPoke.moveSet.move1 != "-"){
+									movelist += tempPoke.moveSet.move1.name;
+								}
+								if (tempPoke.moveSet.move2 != "-"){
+									movelist += tempPoke.moveSet.move2.name;
+								}
+								if (tempPoke.moveSet.move3 != "-"){
+									movelist += tempPoke.moveSet.move3.name;
+								}
+								if (tempPoke.moveSet.move4 != "-"){
+									movelist += tempPoke.moveSet.move4.name;
+								}
+							}
+							logger.info("[move-assist] Got moves from Pokemon")
+						});
+				}
+			})
+	});
+	//Variables to aid in search
+	let move = '';
+	let found = false;
+
+	while (!found) {
+		//Get a random index to pull a random move
+		let moveindex = Math.floor(Math.random() * (movelist.length - 1));
+		move = movelist[moveindex];
+		let valid = true;
+		let i = 0;
+		//Test to see if random move meets criteria
+		while (valid && i < assistunselectable.length){
+			if(move === assistunselectable[i]){
+				valid = false;
+				logger.info('[move] Selected move not allowed, retrying');
+			}
+			i++;
+		}
+		if (valid){
+			found = true;
+			logger.info('[move] Found a move meeting all criteria');
+		}
 	}
-};
+	interaction.followUp("Your assist calls " + move);
+
+	};
+}
 
 function sleep(ms) {
 	return new Promise((resolve) => {
