0ef8709e019ad2e61c37e9dc28da634430eada83
Talmanaze <j.zyborg@gmail.com>
2023-08-09 19:31:00 -0400

    Added Beatup message

--- commands/move.js
+++ commands/move.js
@@ -172,7 +172,7 @@ module.exports.run = async (interaction) => {
 			});
 	} else if (interaction.options.getSubcommand() === 'beatup') {
 		let Pokemon = require(`../models/pokemon`);
-        let tempPoke = new Pokemon;
+        
 
 		let followup = "";
 		let inputstring = interaction.options.getString('beatuppokemon');
@@ -180,6 +180,7 @@ module.exports.run = async (interaction) => {
 		let promisearray = [];
 
 		names.forEach((element) => {
+			let tempPoke = new Pokemon;
 			let notFoundMessage = element + " not found. Please check that you entered the name properly (case-sensitive) and try again.\n\n(Hint: use `+listpoke` to view the Pokemon you can edit.)";
 			let sql = `SELECT * FROM pokemon WHERE name = '${element}';`;
 			logger.info('[move-beatup] SQL query: ${sql}');
@@ -195,7 +196,7 @@ module.exports.run = async (interaction) => {
 					if (response[0].private > 0 && interaction.user.id !== response[0].discordID) {
 						logger.info("[modpoke] Detected user attempting to access private Pokemon.")
 						// If user found a pokemon that was marked private and belongs to another user, act as if the pokemon doesn't exist in messages
-						interaction.reply(notFoundMessage);
+						interaction.followUp(notFoundMessage);
 						return;
 					}
 
@@ -203,7 +204,7 @@ module.exports.run = async (interaction) => {
 						.then(response => {
 	
 							logger.info("[move-beatup] Got Pokemon info.");
-							let math = tempPoke.statBlock.finalStats[1]/10;
+							let math = tempPoke.statBlock.baseStats[1]/10;
 							math += 5;
 							followup += "Beat Up base power for " + element + " is " + math + ".\n";
 	
@@ -214,7 +215,7 @@ module.exports.run = async (interaction) => {
 			
 	});
 	Promise.all(promisearray).then(() => {
-		interaction.followUp(followup);
+		interaction.followUp(followup + "This is the base power each strike should use with the Damage command - each strike can crit and STAB individually.");
 	  })
 	}else if (interaction.options.getSubcommand() === 'assist') {
 		let Pokemon = require(`../models/pokemon`);
