effb403cf33d5e069a9f9a8fff93ef428d659cb9
Natalie Landsberg <natalie.landsberg97@gmail.com>
2023-10-11 19:52:10 -0400

    try to get damage working

---
+++ views/damage.js
@@ -0,0 +1,37 @@
+function calculateDamage() {
+    const dmgCall = require("../commands/damage.js")
+    var attacker = document.getElementById("attackerName")
+    var defender = document.getElementById("defenderName")
+    var moveName = document.getElementById("moveName")
+
+    mysqlConnection = mysql.createConnection({
+        host: config.mysql_host,
+        user: config.mysql_user,
+        password: config.mysql_pass,
+        database: config.mysql_db,
+        port: config.mysql_port,
+        supportBigNumbers: true,
+        bigNumberStrings: true
+      });
+    mysqlConnection.connect();
+
+    interaction = {
+        client : mysqlConnection,
+        deferReply : function() {},
+        options : {
+            getSubcommand : function() {},
+            "attacker-name" : attacker,
+            "defender-name" : defender,
+            "move-name" : moveName
+        },
+        editReply : function() {},
+        followUp : function() {},
+        channel : {
+            send : function() {}
+        }
+
+        
+    }
+
+    dmgCall.run(interaction)
+}
