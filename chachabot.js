00525392b32b2b425e16e0384db8f6469b77ceb6
Talmanaze <j.zyborg@gmail.com>
2023-12-06 19:14:09 -0500

    Initial Autocomplete for PokemonNames

--- chachabot.js
+++ chachabot.js
@@ -69,6 +69,23 @@ fs.readFile('data/Abilities.txt', 'utf8', (err, data) => {
     client.abilitylist = abilitylistArray;
 })
 
+// ================= SQL statements  =================
+        // sql statement to check if the Pokemon exists
+let sqlFindPoke = `SELECT * FROM pokemon`;
+let pokemonCacheArray
+logger.info(`[startup] SQL find pokemon query: ${sqlFindPoke}`);
+
+// try to find the poke in the array first
+client.mysqlConnection.query(sqlFindPoke, function (err, rows) {
+   // if you're here, the name couldn't be found in the table
+    if (err) {
+        let cantAccessSQLMessage = "SQL error, please try again later or contact a maintainer if the issue persists.";
+        logger.error("[startup]" + cantAccessSQLMessage + ` ${err}`)
+        return;
+    }
+    pokemonCacheArray = rows;
+    client.pokemonCache = pokemonCacheArray;
+})
 //Load each command
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
