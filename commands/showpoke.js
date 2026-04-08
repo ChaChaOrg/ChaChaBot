0a3f01029441029c911109557999c048575fb27d
CheetahX24 <wsauten@hotmail.com>
2026-04-08 19:38:11 -0400

    Fixed member ID check

--- commands/showpoke.js
+++ commands/showpoke.js
@@ -69,7 +69,7 @@ module.exports.run = async (interaction) => {
             else {
                 // check if the user is allowed to edit the Pokemon. If a Pokemon is private, the user's discord ID must match the Pokemon's creator ID
                 if (response[0].private > 0 && interaction.member.user.id !== response[0].discordID) {
-                    logger.info("[modpoke] Detected user attempting to edit private Pokemon that isn't their own.")
+                    logger.info("[showpoke] Detected user attempting to edit private Pokemon that isn't their own.")
                     // If user found a pokemon that was marked private and belongs to another user, act as if the pokemon doesn't exist in messages
                     interaction.editReply(notFoundMessage);
                     return;
@@ -90,7 +90,7 @@ module.exports.run = async (interaction) => {
 
     } catch (error) {
         logger.error("[showpoke] " + error.toString());
-        interaction.channel.send(error.toString());
-        interaction.channel.send('ChaCha machine :b:roke, please try again later').catch(console.error);
+        interaction.followUp(error.toString());
+        interaction.followUp('ChaCha machine :b:roke, please try again later').catch(console.error);
     }
 };
